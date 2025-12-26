3d5427b51be217169642e1f564fd8bca535e02e0
A-D-Line <67552789+A-D-Line@users.noreply.github.com>
2025-12-26 01:21:33 +0000

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Contact: React.FC = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = '//embed.typeform.com/next/embed.js';
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
         <section className="py-24 px-8 bg-soft-pink w-full">
             <div className="max-w-xl mx-auto">
-                <h2 className="text-4xl font-bold mb-8 text-black text-center">Contact</h2>
+                <h2 className="text-4xl font-bold mb-8 text-black text-center">
+                    Contact
+                </h2>
 
                 <p className="text-lg text-dark-grey text-center mb-12">
-                    I work full time and I am not currently taking on any new projects. <br/><br/>
-                    You can find me on LinkedIn <a href="https://www.linkedin.com/in/adam-line-9688a624a/" target="_blank"><u>here</u></a>.
+                    I work full time and I am not currently taking on any new projects.
+                    <br /><br />
+                    You can find me on LinkedIn{' '}
+                    <a
+                        href="https://www.linkedin.com/in/adam-line-9688a624a/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <u>here</u>
+                    </a>.
                 </p>
 
-                <div data-tf-live="01KDC35C886WXGBP17411ZWTK5"></div>
-                
+                {/* Typeform embed */}
+                <div
+                    data-tf-live="01KDC35C886WXGBP17411ZWTK5"
+                    className="mt-16"
+                ></div>
+
                 <div className="mt-16 text-sm text-dark-grey text-center opacity-60">
                     &copy; {new Date().getFullYear()} Adam Line. All rights reserved.
                 </div>
